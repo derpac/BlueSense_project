1b3b354bf5556a67f8c879983a8b8da9e1790d7f
derpac <derry.packer2@gmail.com>
2025-07-08 10:44:28 +0100

    move some files about

--- basic-plots/debug.py
+++
@@ -1,5 +0,0 @@
-df = pd.read_csv(file_path, header=None)
-
-# Check the number of columns and inspect the data
-print(df.shape)  # Print the shape to see how many rows and columns are in the data
-print(df.head())  # Print the first few rows to visually inspect the data
--- calibration/calibration.py
+++
@@ -1,6 +0,0 @@
-# run a calibration test 
-
-# use the data and find the min and max 
-
-# Calibration code exists on my laptop... time to sync this to github
-
